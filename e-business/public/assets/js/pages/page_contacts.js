146a11c1dccbb257c0763d14bee1cc3298c16b4c
ouikou <wang.weiguang@zipc.com>
2017-04-04 16:16:45 +0900

    0404 - 16

--- e-business/public/assets/js/pages/page_contacts.js
+++ e-business/public/assets/js/pages/page_contacts.js
@@ -1,23 +1,28 @@
 var ContactPage = function () {
 
     return {
-        
+
     	//Basic Map
         initMap: function () {
 			var map;
 			$(document).ready(function(){
 			  map = new GMaps({
 				div: '#map',
-				scrollwheel: false,				
-				lat: 40.748866,
-				lng: -73.988366
+				scrollwheel: false,
+        lat: 35.662919,
+        lng: 139.756533,
+        zoom: 17
 			  });
-			  
+
 			  var marker = map.addMarker({
-				lat: 40.748866,
-				lng: -73.988366,
-	            title: 'Company, Inc.'
-		       });
+          lat: 35.662919,
+          lng: 139.756533,
+          animation: google.maps.Animation.DROP,
+          title: 'ShineSoft Co.,Ltd.',
+          infoWindow: {
+            content: "東京都港区新橋5-12-6 ウェルディ新橋4F"
+          }
+		      });
 			});
         },
 
@@ -27,11 +32,11 @@ var ContactPage = function () {
 		    $(document).ready(function(){
 		      panorama = GMaps.createPanorama({
 		        el: '#panorama',
-		        lat : 40.748866,
-		        lng : -73.988366
+            lat: 35.662919,
+            lng: 139.756533
 		      });
 		    });
-		}        
+		}
 
     };
 }();
